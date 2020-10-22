db8518fc6eb9948e5bc48462f9db8b5e8e0efca2
Batsheva <bat211735626@gmail.com>
2020-10-22 15:16:39 +0300

    הדפסה ראשונה-בת שבע

---
+++ doNet_0787/doNet_0787/Program5626.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doNet_0787
+{
+    partial class Program
+    {
+        static partial void Welcome5626()
+        {
+            Console.WriteLine("hi i am here too");
+        }
+    }
+}
