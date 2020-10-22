276dadef13d84adb2c56da36384fa54b9f0a86fe
lea haimovich <haimovit@g.jct.ac.il>
2020-10-22 12:38:36 +0300

    add-check

--- doNet_0787/doNet_0787/Program0787.cs
+++ doNet_0787/doNet_0787/Program0787.cs
@@ -14,11 +14,12 @@ namespace doNet5781_00_0787_5626
             Welcome5626();
             Console.ReadKey();
         }
-        static partial void Welcome5626();
+        static partial void Welcome5626();//batchi function
 
-        private static void Welcome0787()
+        private static void Welcome0787()//lea fuction
         {
             Console.WriteLine("Enter your name: ");
+            Console.WriteLine("pleas! ");
             string name = Console.ReadLine();
             Console.WriteLine("{0}, welcome to my first console application", name);
         }
