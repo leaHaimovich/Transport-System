b5c71491e1414d75e29e87ffa51462f1604cb639
Batsheva <bat211735626@gmail.com>
2020-10-22 15:53:30 +0300

    hi2

--- doNet_0787/doNet_0787/Program5626.cs
+++ doNet_0787/doNet_0787/Program5626.cs
@@ -8,6 +8,10 @@ namespace doNet_0787
 {
     partial class Program
     {
+        static void Main(string[] args)
+        {
+            Welcome5626();
+        }
         static partial void Welcome5626()
         {
             Console.WriteLine("i am also here");
